28402fda267a4a9ed6fde2d202790c8e922dfb11
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-17 10:56:11 +0530

    Create guvi30.java

---
+++ guvi30.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int hr1=s.nextInt();
+		int min1=s.nextInt();
+int hr2=s.nextInt();
+
+	int min2=s.nextInt();
+System.out.println(Math.abs(hr1-hr2)+" "+Math.abs(min1-min2));
+
+}
+}
